d1efbcd6f1def0cb488a867d0909388f0ddcad37
Thalyson Wiker <thalison.wilker@gmail.com>
2023-05-10 09:54:56 -0300

    atualização

--- app.py
+++ app.py
@@ -1,20 +1,18 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
-import pandas as pd
 from io import StringIO
 
 st.title('Visualizador de CSV')
-
-uploaded_file = st.file_uploader("Choose a file",type=["csv"], help="Carregue aqui o arquivo CSV")
+delimiter = st.selectbox('Escolha o delimitador', (';', ',',))
+uploaded_file = st.file_uploader(
+    "Choose a file", type=["csv"], help="Carregue aqui o arquivo CSV")
 if uploaded_file is not None:
+
     lines = uploaded_file.readlines()
-
-    header = [ n.lower() for n in lines[:1][0].decode('latin-1').split(";") ]
-
+    header = [n.lower() for n in lines[:1][0].decode('latin-1').split(delimiter)]
     data = []
 
     for line in lines[1:50]:
-        data.append(set( [ data for data in line.decode('latin-1').split(";") ] ))
+        data.append(set([data for data in line.decode('latin-1').split(delimiter)]))
 
-    st.table(pd.DataFrame(data,columns=header))
+    st.table(pd.DataFrame(data, columns=header))
