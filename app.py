b19cf2f480becc1431cb1d00f53e954b58d66ebf
Thalyson Wiker <thalison.wilker@gmail.com>
2023-05-09 21:59:08 -0300

    retornar a restrinção de tamanho

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ if uploaded_file is not None:
 
     data = []
 
-    for line in lines[1]:
+    for line in lines[1:50]:
         data.append(set( [ data for data in line.decode('latin-1').split(";") ] ))
 
     st.table(pd.DataFrame(data,columns=header))
