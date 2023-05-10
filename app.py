6cf5271e7e31349cf145d87b5d8f33861a35de36
Thalyson Wiker <thalison.wilker@gmail.com>
2023-05-09 21:53:01 -0300

    initial commit

---
+++ app.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import pandas as pd
+from io import StringIO
+
+st.title('Visualizador de CSV')
+
+uploaded_file = st.file_uploader("Choose a file",type=["csv"], help="Carregue aqui o arquivo CSV")
+if uploaded_file is not None:
+    lines = uploaded_file.readlines()
+
+    header = [ n.lower() for n in lines[:1][0].decode('latin-1').split(";") ]
+
+    data = []
+
+    for line in lines[1:10]:
+        data.append(set( [ data for data in line.decode('latin-1').split(";") ] ))
+
+    st.table(pd.DataFrame(data,columns=header))
